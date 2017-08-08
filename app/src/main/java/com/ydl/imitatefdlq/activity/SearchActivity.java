a3a299bca47857ab1bee9d9bfb0aa9b376d9627a
qweenhool <qweenhool@gmail.com>
2017-08-08 21:24:59 +0800

    再试一下

--- app/src/main/java/com/ydl/imitatefdlq/activity/SearchActivity.java
+++ app/src/main/java/com/ydl/imitatefdlq/activity/SearchActivity.java
@@ -12,9 +12,5 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        //Todo 搜索界面
-        if(true){
-            
-        }
     }
 }
